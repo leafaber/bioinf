9a945b7fcfa0c33744bd2895f8f35278b8945aa9
Luka Mucko <luka.mucko@fer.hr>
2023-04-06 16:02:03 +0200

    Fix: Ignore heading in .fna

--- src/countKmers.cpp
+++ src/countKmers.cpp
@@ -1,3 +1,7 @@
+/*************************************************************************************************************************
+ *  This code uses LDCF object to count k-mers in a given genome by counting the number of unique additions into ldcf    *
+ *************************************************************************************************************************/
+
 #include <filesystem>
 #include <iostream>
 #include <fstream>
@@ -7,7 +11,10 @@
 #include "LDCF.h"
 using namespace std;
 
-
+/*********************************************************************************************************************************
+*   Read genome by k-mers and add them to ldcf if it's not already in it. Returns the number of insertions i.e. unique k-mers    *
+*   author: Luka Mucko                                                                                                           *
+/*********************************************************************************************************************************/
 int count_k_mers(LDCF* ldcf, string genome, int k){
     ifstream file(genome);
     if (!file.is_open()){
@@ -22,28 +29,32 @@ int count_k_mers(LDCF* ldcf, string genome, int k){
     kmer[k] = '\0';
     auto start = chrono::high_resolution_clock::now();
 
-    int i=0;
     char c;
+    int i;
     while(file){
         streampos pos = file.tellg();
         i=0;
         while(file.get(c) && i<k){
-            if(c!='\n'){
+            if(c!='\n' && c!='>'){
                 kmer[i]=c;
                 i++;
-            }
+            }else if (c=='>')
+            {
+                getline(file,delim);
+                pos = file.tellg();
+                i=0;
+            }            
         }
-        /*if(i<k){
-            auto end = chrono::high_resolution_clock::now();
-            auto time_ms = chrono::duration_cast<chrono::milliseconds>(end-start);
-            cout << "Done reading in: " << time_ms.count() << "ms\n";
-            return unique_kmers;
-        }*/
-        unique_kmers += ldcf->insert(string(kmer));
+        if(i<k){
+            break;
+        }
+        if(!ldcf->search(string(kmer))){
+            unique_kmers+=ldcf->insert(string(kmer));
+        }
         file.seekg(pos);
         file.seekg(1, ios::cur);
     }
-    
+
     auto end = chrono::high_resolution_clock::now();
     auto time_ms = chrono::duration_cast<chrono::milliseconds>(end-start);
     cout << "Done reading in: " << time_ms.count() << "ms\n";
